6e598b1cdd3fe211c905a8580ac0c2fba7f7a12d
alexrewrew <alexrewrew@live.com>
2016-10-05 17:28:35 +0300

    first commit

---
+++ assets/js/plugins.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.icheck_input').iCheck({
+        checkboxClass: 'icheckbox_square'
+        , radioClass: 'iradio_square'
+        , increaseArea: '20%' // optional
+    });
+    /* WOW.JS */
+    new WOW().init();
+});
---
+++ assets/js/sliders.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    /* SLICK */
+    $('.your-class').slick({
+    });
+});
