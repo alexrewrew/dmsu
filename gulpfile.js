14948fe5df8ffe620a1098a75c1bcdd6c2b32b54
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-03-28 11:23:43 +0300

    Gulp-menu

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,32 @@ gulp.task('sass', function () {
             browsers: ['last 40 versions'],
             cascade: false
         }))
-
+        // .pipe(csscomb({
+        //     "remove-empty-rulesets": true,
+        //     "always-semicolon": true,
+        //     "color-case": "lower",
+        //     "block-indent": "\t",
+        //     "color-shorthand": true,
+        //     "element-case": "lower",
+        //     "eof-newline": true,
+        //     "leading-zero": false,
+        //     "quotes": "single",
+        //     "sort-order-fallback": "abc",
+        //     "space-before-colon": "",
+        //     "space-after-colon": " ",
+        //     "space-before-combinator": " ",
+        //     "space-after-combinator": " ",
+        //     "space-between-declarations": "\n",
+        //     "space-before-opening-brace": " ",
+        //     "space-after-opening-brace": "\n",
+        //     "space-after-selector-delimiter": " ",
+        //     "space-before-selector-delimiter": "",
+        //     "space-before-closing-brace": "\n",
+        //     "strip-spaces": true,
+        //     "tab-size": true,
+        //     "unitless-zero": true,
+        //     "vendor-prefix-align": true
+        // }))
         .pipe(gulp.dest('app/css')) //вивід зкомпільованого файлу до теки css
         .pipe(browserSync.reload({ // перезавантажувати синхронізатор при кожній зміні
             stream: true
